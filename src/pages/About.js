8deaa37d7f2ed801ad9ae39842e6b91c2881813a
Garrett Sullivan <gsullivan7402@gmail.com>
2024-05-21 17:24:32 -0400

    added Header and footer, added routes for about page, home page, and contact page

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About Corate</h1>
+            <p>Welcome to Corate, your ultimate website builder designed to simplify your web creation experience.</p>
+            <p>Our mission is to empower individuals and small businesses to create professional and visually appealing websites without the need for extensive technical knowledge or resources.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>If you have any questions, please don't hesitate to reach out to us.</p>
+            <p>Email: info@corate.com</p>
+        </div>
+    );
+}
+
+export default Contact;
